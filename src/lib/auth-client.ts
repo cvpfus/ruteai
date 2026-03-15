8fce98680ad2dd43f2f25c41f2e2c77fbede4437
cvpfus <47532266+cvpfus@users.noreply.github.com>
2026-03-15 12:05:43 +0700

    refactor: restructure auth client and add server auth module

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,3 +1,7 @@
 import { createAuthClient } from 'better-auth/react'
+import { convexClient } from '@convex-dev/better-auth/client/plugins'
+import { apiKeyClient } from '@better-auth/api-key/client'
 
-export const authClient = createAuthClient()
+export const authClient = createAuthClient({
+  plugins: [convexClient(), apiKeyClient()],
+})
